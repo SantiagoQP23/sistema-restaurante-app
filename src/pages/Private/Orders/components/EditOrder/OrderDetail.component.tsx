b78b222eb4a72954519cbe41995bb7512a52d058
Santiago Quirumbay <santiagoquirumbay10@gmail.com>
2023-06-03 08:42:01 -0500

    update order detail

--- src/pages/Private/Orders/components/EditOrder/OrderDetail.component.tsx
+++ src/pages/Private/Orders/components/EditOrder/OrderDetail.component.tsx
@@ -22,6 +22,7 @@ import { selectOrders } from '../../../../../redux/slices/orders/orders.slice';
 import { statusModalDescriptionDetail, statusModalEditOrderDetail } from '../../services/orders.service';
 import { DeleteOrderDetailDto } from '../../dto/delete-order-detail.dto';
 import { styled, useTheme } from '@mui/material/styles';
+import { useUpdateOrderDetail } from '../../hooks/useUpdateOrderDetail';
 
 
 interface Props {
@@ -58,12 +59,14 @@ export const OrderDetail: FC<Props> = ({ detail }) => {
 
   const { enqueueSnackbar } = useSnackbar();
 
+  const {update, loading } = useUpdateOrderDetail();
+
   const editDescription = () => {
     statusModalDescriptionDetail.setSubject(true, detail);
   }
 
 
-  const updateQuantityDetail = () => {
+  const updateQuantity = () => {
 
     const data: UpdateOrderDetailDto = {
       orderId: activeOrder!.id,
@@ -71,17 +74,7 @@ export const OrderDetail: FC<Props> = ({ detail }) => {
       quantity: counter
     }
 
-    socket?.emit(EventsEmitSocket.updateOrderDetail, data, ({ ok, order, msg }: SocketResponseOrder) => {
-
-      if (ok) {
-        dispatch(setActiveOrder(order!))
-
-      } else {
-        enqueueSnackbar(msg, { variant: 'error' });
-      }
-
-    });
-
+    update(data);
 
   }
 
@@ -153,7 +146,7 @@ export const OrderDetail: FC<Props> = ({ detail }) => {
                 color='primary'
                 onClick={(e) => {
                   e.stopPropagation();
-                  updateQuantityDetail()
+                  updateQuantity()
                 }}
               >
                 <SaveOutlined />
@@ -209,171 +202,7 @@ export const OrderDetail: FC<Props> = ({ detail }) => {
           </IconButton>
         </TableCell>
 
-
-
-
-
-
-
-
       </TableRow>
-
-      {/* <Box
-        sx={{
-         p: 1,
-          borderRadius: '5px',
-          // border: `1px solid ${
-
-          //   detail.qtyDelivered === detail.quantity ? theme.colors.success.main : theme.colors.warning.main
-          //   }`,
-          // color: `${
-
-          //   detail.qtyDelivered === detail.quantity ? theme.colors.success.main : theme.colors.warning.main
-          //   }`,
-
-
-          '&:hover': {
-            backgroundColor: 'rgba(0,0,0,0.05)',
-            cursor: 'pointer'
-          }
-
-        }}
-        onClick={editDetail}
-      >
- 
-
-        <Box
-          sx={{
-            display: 'flex', justifyContent: 'space-between', gap: 1,  alignItems: 'center',
-          }}
-        >
-
-
-
-          <Typography
-            variant="h5"
-
-          >
-
-            {detail.product.name} - $ {detail.product.price}
-
-
-       
-          </Typography>
-
-          <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-              <CircularProgress
-                variant="determinate"
-                size={25}
-                value={detail.qtyDelivered * 100 / detail.quantity}
-                sx={{ color: detail.qtyDelivered === detail.quantity ? 'success.main' : 'warning.main' }}
-              />
-              <Box
-                sx={{
-                  top: 0,
-                  left: 0,
-                  bottom: 0,
-                  right: 0,
-                  position: 'absolute',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                <Typography
-                  variant="caption"
-                  component="div"
-                  sx={{ color: detail.qtyDelivered === detail.quantity ? 'success.main' : 'warning.main' }}
-
-                >{detail.qtyDelivered}</Typography>
-              </Box>
-            </Box>
-
-
-
-        </Box>
-
-
-        <Box sx={{ display: "flex" }}>
-          <Box sx={{ flexGrow: 1 }} >
-
-            <Typography
-              variant="body2"
-              color={detail.description ? "darkslateblue" : ""}
-              style={{ whiteSpace: 'pre-wrap' }}
-            >
-              {detail.description}
-
-            </Typography>
-
-
-          </Box>
-
-
-        </Box>
-
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-
-          <Box display='flex' justifyContent='space-between' alignItems='center'>
-
-
-            <IconButton
-              onClick={
-                (e) => {
-                  e.stopPropagation();
-                  decrement()
-
-                }
-              
-              }
-            >
-              <RemoveCircleOutline />
-            </IconButton>
-
-            <Typography sx={{ width: 40, textAlign: 'center' }}>{counter}</Typography>
-            <IconButton
-              onClick={
-                (e) => {
-                  e.stopPropagation();
-                  increment()
-
-                } 
-                
-              }
-            >
-              <AddCircleOutline />
-            </IconButton>
-            {
-              counter !== detail.quantity && counter > 0 && counter >= detail.qtyDelivered &&
-              <IconButton
-                disabled={!counter || counter === detail.quantity || counter < detail.qtyDelivered}
-                color='primary'
-                onClick={(e) => {
-                  e.stopPropagation();
-                  updateQuantityDetail()
-                }}
-              >
-                <SaveOutlined />
-              </IconButton>
-
-            }
-
-
-          </Box>
-
-          <Typography variant="body1" textAlign='right' fontWeight='bold'>$ {detail.amount}</Typography>
-
-
-        </Box> */}
-      {/* <LinearProgressWrapper
-          value={(detail.qtyDelivered * 100) / detail.quantity}
-          color="primary"
-          variant="determinate"
-        /> */}
-
-      {/* </Box> */}
-
-
 
     </>
   )
