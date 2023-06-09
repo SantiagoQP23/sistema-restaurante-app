941d5fb8a06f260fc439c7a7bfeeba0a1006ddcb
Santiago Quirumbay <santiagoquirumbay10@gmail.com>
2023-06-08 21:31:56 -0500

    ui improve

--- src/pages/Private/Menu/router/Menu.router.tsx
+++ src/pages/Private/Menu/router/Menu.router.tsx
@@ -3,11 +3,11 @@ import { RouteObject } from 'react-router-dom';
 
 import { PrivateRoutes } from '../../../../models/routes.model';
 
-const Menu = lazy(() => import('../../Orders/views/Menu/Menu.view'))
+// const Menu = lazy(() => import('../../Orders/views/Menu/Menu.view'))
 
-export const MenuRouter: RouteObject = {
+// export const MenuRouter: RouteObject = {
 
-  path: PrivateRoutes.MENU,
-  element: <Menu />
+//   path: PrivateRoutes.MENU,
+//   element: <Menu />
 
-}
+// }
